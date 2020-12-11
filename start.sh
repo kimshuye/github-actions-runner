aff94cd0e395eed6df7c0010319aacd9a82f3ae0
Suntisuk Khemphed <suntisuk.go@gmail.com>
2020-12-11 21:08:35 +0700

    Add REG_TOKEN env

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ REG_TOKEN=$REG_TOKEN
 
 # REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
-echo env
+echo "${env}"
 
 ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
 
