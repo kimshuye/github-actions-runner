0703b8d3e4980f296e5fa956b219fd274ace8564
Suntisuk Khemphed <suntisuk.go@gmail.com>
2020-12-11 21:55:14 +0700

    Update Start bash

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$ACCESS_TOKEN
 REG_TOKEN=$REG_TOKEN
 
-# REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
 echo "${env}"
 
